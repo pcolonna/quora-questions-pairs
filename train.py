db845f8f45df72cd0a735c06ada35740251aebd8
pcolonna <pierre.col.di@gmail.com>
2017-05-24 15:21:14 +0200

    adding embeddings

--- train.py
+++ train.py
@@ -9,6 +9,16 @@ from keras.layers import Dense, Input, Flatten
 from keras.layers import Conv1D, MaxPooling1D, Embedding
 from keras.models import Model
 
-BASE_DIR  = ''
-EMBEDDING = BASE_DIR + '/GoogleNews-Vectors/'
+BASE_DIR  = '.'
+EMBEDDING = BASE_DIR + '/embeddings/' #'/GoogleNews-Vectors/'
 DATA_DIR  = BASE_DIR + '/data/'
+
+def word2vec_embedding_layer(embeddings_path):
+    weights = np.load(open(embeddings_path, 'rb'))
+    layer   = Embedding(input_dim=weights.shape[0], output_dim=weights.shape[1], weights=[weights])
+    return layer
+
+if __name__ == '__main__':
+	input_vec = Input(shape=(300,), dtype='int32', name='input')
+	embedding = word2vec_embedding_layer(EMBEDDING + 'GoogleNews-vectors-negative300.bin.gz')
+	embedded = embedding(input_vec)
