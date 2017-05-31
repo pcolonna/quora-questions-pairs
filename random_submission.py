548b5c1d55931650bcaa54cc2b14058942e55f8e
pcolonna <pierre.col.di@gmail.com>
2017-05-31 21:20:14 +0200

    Add proper proper probabilities. Plus clean up

--- random_submission.py
+++ random_submission.py
@@ -3,50 +3,61 @@
 
 import random
 
-# create random, 50/50, submission
 def create_random_submission():
 
-	line_nb = 0
+    with open('submission.csv','w') as f:
+        f.write('test_id,is_duplicate')
+        f.write('\n')
+        for i in range(2345796):
+                f.write(str(i)+ ',' + str(float("{0:.1f}".format(random.random()))) + '\n')  # add a random probability
 
-	with open('submission.csv','w') as f:
-		f.write('test_id,is_duplicate')
-		f.write('\n')
-		#wrong number of line in my test file somehow.
-		"""with open('test.csv','r') as t:
-			for line in t:
-				f.write(str(line_nb)+ ',' + str(random.getrandbits(1))+'\n')
-				#f.write()
-				line_nb += 1
-		"""
-		#wanted number of prediction.
-		for i in range(2345796):											
-				f.write(str(i)+ ',' + str(random.getrandbits(1))+'\n')
+# create a random submission, 50/50 ones or zeros
+def create_random_submission_1_0():
 
-# find number of duplicate in the file.
-def sum_duplicate():
-	result = 0
-	with open('train.csv','r') as t:
-			for line in t:
-				#print(line[-3])
-				if line[-3] == '1':
-					result += 1
-	
-	return result
+    line_nb = 0
+
+    with open('submission.csv','w') as f:
+        f.write('test_id,is_duplicate')
+        f.write('\n')
+
+        # wrong number of line in my test file somehow.
+        """with open('test.csv','r') as t:
+            for line in t:
+                f.write(str(line_nb)+ ',' + str(random.getrandbits(1))+'\n')
+                #f.write()
+                line_nb += 1
+        """
+        # expected number of prediction.
+        for i in range(2345796):                                            
+                f.write(str(i)+ ',' + str(random.getrandbits(1))+'\n')
 
 # create random submission.
 # respect probability to find a duplicate, according to training set.
-def create_random_submission_biased():
+def create_random_submission_1_0_biased():
 
-	line_nb = 0
-	proba   = sum_duplicate() / 2345796
+    proba   = sum_duplicate() / 2345796
 
-	with open('submission.csv','w') as f:
-		f.write('test_id,is_duplicate')
-		f.write('\n')
-		for i in range(2345796):
-				prediction = int(random.random() < proba)
-				f.write(str(i)+ ',' + str(prediction)+'\n')
+    with open('submission.csv','w') as f:
+        f.write('test_id,is_duplicate')
+        f.write('\n')
+        for i in range(2345796):
+                prediction = int(random.random() < proba)
+                f.write(str(i)+ ',' + str(prediction)+'\n')
+
+
+
+# find number of duplicate in the file.
+def sum_duplicate():
+    result = 0
+    
+    with open('train.csv','r') as t:
+            for line in t:
+                if line[-3] == '1':
+                    result += 1
+    
+    return result
 
 if __name__ == "__main__":
-	create_random_submission_biased()
-	
+    create_random_submission_biased()
+    #sum_duplicate()
+    
