dee4dea38d60e8fc3faec48041fcb8116535ef36
pcolonna <pierre.col.di@gmail.com>
2017-05-31 17:35:59 +0200

    Modified code to generate random submission. add bias.

--- random_submission.py
+++ random_submission.py
@@ -3,6 +3,7 @@
 
 import random
 
+# create random, 50/50, submission
 def create_random_submission():
 
 	line_nb = 0
@@ -10,24 +11,42 @@ def create_random_submission():
 	with open('submission.csv','w') as f:
 		f.write('test_id,is_duplicate')
 		f.write('\n')
-
+		#wrong number of line in my test file somehow.
 		"""with open('test.csv','r') as t:
 			for line in t:
 				f.write(str(line_nb)+ ',' + str(random.getrandbits(1))+'\n')
 				#f.write()
 				line_nb += 1
 		"""
-		for i in range(2345796):
+		#wanted number of prediction.
+		for i in range(2345796):											
 				f.write(str(i)+ ',' + str(random.getrandbits(1))+'\n')
 
+# find number of duplicate in the file.
 def sum_duplicate():
 	result = 0
 	with open('train.csv','r') as t:
 			for line in t:
-				if line[0] == '1':
+				#print(line[-3])
+				if line[-3] == '1':
 					result += 1
-	print(result)
+	
+	return result
+
+# create random submission.
+# respect probability to find a duplicate, according to training set.
+def create_random_submission_biased():
+
+	line_nb = 0
+	proba   = sum_duplicate() / 2345796
+
+	with open('submission.csv','w') as f:
+		f.write('test_id,is_duplicate')
+		f.write('\n')
+		for i in range(2345796):
+				prediction = int(random.random() < proba)
+				f.write(str(i)+ ',' + str(prediction)+'\n')
 
 if __name__ == "__main__":
-	#create_random_submission()
-	sum_duplicate()
+	create_random_submission_biased()
+	
